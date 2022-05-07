4d46468a48deaafb10bcdcd5646906663475e476
Aaron Elias <aaronelias007@gmail.com>
2022-05-07 19:42:04 -0400

    build recipe card tile and class

--- src/components/Items/RecipeCard.js
+++ src/components/Items/RecipeCard.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 
-const RecipeCard = () => {
+const RecipeCard = ({meal}) => {
   return (
     <div>
-      
-    </div>
+      <div className="card">
+        <div className="cardBody">
+          <h4 className="card-title">{meal.strMeal}</h4>
+        
+        
+      </div>
+      </div> 
+      </div>
   )
 }
 
 export default RecipeCard
+
+// function Stock({ stock, handleClick }) {
+//   return (
+//     <div>
+//       <div className="card" onClick={() => handleClick(stock)}>
+//         <div className="card-body">
+//           <h5 className="card-title">{stock.name}</h5>
+//           <p className="card-text">{`${stock.ticker}: ${stock.price}`}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Stock;
