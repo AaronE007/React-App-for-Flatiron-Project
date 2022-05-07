791336d928a4547b8cca6af47ce7bee598f94dff
Aaron Elias <aaronelias007@gmail.com>
2022-05-06 20:06:48 -0400

    add image to the home page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ import React from "react";
 export const About = () => {
   return (
     <div> style={{width: "100%"}}
-     <p style={{fontSize:"1em", fontWeight: "400"}}> This is a website is for all seafood eaters and pescatarians</p>
+     <p style={{fontSize:"1em", fontWeight: "400"}}> 
+      This is a website is for all seafood eaters and pescatarians. 
+      Feel free to scroll through the recipes or checkout if there is a specific recipe you want with the search bar.
+      If there are any seafood recipes you would like to add, go ahead and enter them in the New Recipe form. 
+     </p>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-     
+      <img style={{width: "80%"}} src={window.location.origin + "/images/bigfish.jpeg"} alt="A large Marlin fish" />
     </div>
   )
 }
