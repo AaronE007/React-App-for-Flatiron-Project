7c43cf653ebd29d6efd582bdcddfe3fa18cf447a
Aaron Elias <aaronelias007@gmail.com>
2022-05-09 18:35:28 -0400

    Add if statement to the handle submit in order to check if new values exist

--- src/components/Items/Form.js
+++ src/components/Items/Form.js
@@ -7,11 +7,20 @@ const Form = () => {
   const [instructions, setInstructions] = useState("")
 
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    if([recipeName,recipeImage,ingredients,instructions]).some(val => val.trim() === "")){
+      alert("Make sure that all of the vla")
+    }
+  
+  }
+  
+
   
 
   return (
     <div style={{margin: "auto", border: "solid", backgroundColor: "blue", height: 150, width: 400, color: "gold", fontWeight: "superBold"}}>
-      <form >
+      <form onSubmit={handleSubmit}>
       <label htmlFor="RecipeName">Recipe name</label>
       <input onChange={event => setRecipeName(event.target.value)} type="text" name="recipe" id="name" value={recipeName} required/><br />
       <label htmlFor="RecipeImage">Recipe Image</label>
@@ -27,9 +36,9 @@ const Form = () => {
 
 export default Form
 
-
-// "strMeal": "Cajun spiced fish tacos",
-//   "strMealThumb": "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
-//   "idMeal": "52819",
-//   "strIngredients": "2 tbsp cajun 1 tsp cayenne pepper 4 fillets white fish 1 tsp vegetable oil 8 flour tortilla 1 sliced avocado 2 shredded little gem lettuce 4 shredded spring onion 1 x 300ml salsa 1 pot sour cream 1 lemon 1 clove finely chopped garlic",
-//   "strInstructions":
+// This is how the data looks on the server:
+// // "strMeal": "Cajun spiced fish tacos",
+// //   "strMealThumb": "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+// //   "idMeal": "52819",
+// //   "strIngredients": "2 tbsp cajun 1 tsp cayenne pepper 4 fillets white fish 1 tsp vegetable oil 8 flour tortilla 1 sliced avocado 2 shredded little gem lettuce 4 shredded spring onion 1 x 300ml salsa 1 pot sour cream 1 lemon 1 clove finely chopped garlic",
+// //   "strInstructions":
