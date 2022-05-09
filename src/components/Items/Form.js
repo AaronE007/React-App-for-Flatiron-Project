1dc312c79e685323dc33c0c6b1c379f5706979a6
Aaron Elias <aaronelias007@gmail.com>
2022-05-09 19:55:33 -0400

    Add change to state in order to match the keys in the db.json

--- src/components/Items/Form.js
+++ src/components/Items/Form.js
@@ -2,55 +2,58 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const Form = () => {
-  const [recipeName, setRecipeName] = useState("")
-  const [recipeImage, setRecipeImage] = useState("")
-  const [ingredients, setIngredients] = useState("")
-  const [instructions, setInstructions] = useState("")
+  const [strMeal, setstrMeal] = useState("")
+  const [strMealThumb, setRecipeImage] = useState("")
+  const [strIngredients, setstrIngredients] = useState("")
+  const [strInstructions, setstrInstructions] = useState("")
   const hist = useHistory()
   
   
   function handleSubmit(event) {
     event.preventDefault()
-    if([recipeName,recipeImage,ingredients,instructions].some(value => value.trim() === "")){
+    if([strMeal,strMealThumb,strIngredients,strInstructions].some(value => value.trim() === "")){
       alert("Make sure that all of the sections are filled before continuing.")
       return null
     }
-    const newRecipes = {recipeName, recipeImage, ingredients, instructions}
+    const newRecipes = {strMeal: strMeal, strMealThumb: strMealThumb, strIngredients: strIngredients, strInstructions: strInstructions}
 
     fetch("http://localhost:3001/meals", {
       Method: "Post",
-      Headers: { "Content-Type": "application/json"},
+      Headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newRecipes)
 })
- setRecipeName("")
- setRecipeImage("")
- setIngredients("")
- setInstructions("")
-  hist.push("/recipes")
+    setstrMeal("")
+    setRecipeImage("")
+    setstrIngredients("")
+    setstrInstructions("")
+    hist.push("/recipes")
  }
 
 
-  return (
-    <div >
-      <form onSubmit={handleSubmit}>
+return (
+  <div > 
+    <form onSubmit={handleSubmit}>
+      <input type="submit" value="Add a Recipe!" /> 
+      <br />
+      <br />
       <label htmlFor="RecipeName">Recipe name</label>
-      <input onChange={event => setRecipeName(event.target.value)} type="text" name="recipe" id="recipeName" value={recipeName} required/><br />
+      <input onChange={event => strMeal(event.target.value)} type="text" name="recipe" id="recipeName" value={strMeal} required/>
+      <br />
+      <br />
       <label htmlFor="RecipeImage">Recipe Image</label>
-      <input onChange={event => setRecipeImage(event.target.value)} type="text" name="name" id="recipeImage" value={recipeImage} required/><br />
+      <input onChange={event => setRecipeImage(event.target.value)} type="text" name="name" id="recipeImage" value={strMealThumb} required/>
+      <br />
+      <br />
       <label htmlFor="Ingredients">Recipe Ingredients</label>
-      <input onChange={event => setIngredients(event.target.value)} type="text" name="name" id="ingredients" value={ingredients} required/><br />
+      <input onChange={event => setstrIngredients(event.target.value)} type="text" name="name" id="ingredients" value={strIngredients} required/>
+      <br />
+      <br />
       <label htmlFor="Instructions">Recipe Instructions</label>
-      <input onChange={event => setInstructions(event.target.value)} type="text" name="instructions" id="name" value={instructions} required/><br />
-      </form>
-    </div>
+      <input onChange={event => setstrInstructions(event.target.value)} type="text" name="instructions" id="name" value={strInstructions} required/>
+    </form>
+  </div>
   )
 }
 
 export default Form
 
-// This is how the data looks on the server:
-// // "strMeal": "Cajun spiced fish tacos",
-// //   "strMealThumb": "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
-// //   "idMeal": "52819",
-// //   "strIngredients": "2 tbsp cajun 1 tsp cayenne pepper 4 fillets white fish 1 tsp vegetable oil 8 flour tortilla 1 sliced avocado 2 shredded little gem lettuce 4 shredded spring onion 1 x 300ml salsa 1 pot sour cream 1 lemon 1 clove finely chopped garlic",
-// //   "strInstructions":
