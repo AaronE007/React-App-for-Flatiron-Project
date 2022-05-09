c5a60c34beec3714961fe55c7d15e067f2434e71
Aaron Elias <aaronelias007@gmail.com>
2022-05-09 18:31:08 -0400

    Add handle submit to form

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+const style = {
+  width: "100%",
+  magrin:"25px 50px 25px",
+  padding: "1em",
+  color: "darkBlue",
+  backgroundColor: "Gold",
+  fontWeight: "400",
+  verticalAlign: "center",
+  border: "solid"
+
+}
+
 
 const About = () => {
   return (
-    <div style={{width: "80%"}}>
-     <p style={{fontSize:"1em", fontWeight: "400"}}> 
+    <div style={{style}}>
+     <p> 
       This is a website is for all seafood eaters and pescatarians. 
       Feel free to scroll through the recipes or checkout if there is a specific recipe you want with the search bar.
       If there are any seafood recipes you would like to add, go ahead and enter them in the New Recipe form. 
